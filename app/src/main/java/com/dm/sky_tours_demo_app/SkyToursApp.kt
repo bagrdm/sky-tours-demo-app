fcd5a8d0ffb8fde56b60c45a1ad135c4654636f2
BagretsovDm <bagretsovdmitry@gmail.com>
2023-05-25 18:42:42 +0200

    Initial commit

---
+++ app/src/main/java/com/dm/sky_tours_demo_app/SkyToursApp.kt
@@ -0,0 +1,5 @@
+package com.dm.sky_tours_demo_app
+
+import android.app.Application
+
+class SkyToursApp : Application()
---
+++ app/src/main/java/com/dm/sky_tours_demo_app/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dm.sky_tours_demo_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.dm.sky_tours_demo_app.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
